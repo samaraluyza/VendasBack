21000a23ef8d3f03e90e8db6231ee7e8caf35744
samaraluyza <luyzasamara5@gmail.com>
2018-12-04 01:05:54 -0200

    correcao pedidos

--- VendasBack/Controllers/PedidosController.cs
+++ VendasBack/Controllers/PedidosController.cs
@@ -117,7 +117,7 @@ namespace VendasBack.Controllers
         }
 
 
-        [Route("api/Pedidos/{inicio}/{fim}")]
+        [Route("api/PedidosPeriodo/{inicio}/{fim}")]
         [ResponseType(typeof(List<Pedido>))]
         public async Task<IHttpActionResult> GetPedidosPeriodo(DateTime inicio, DateTime fim)
         {
